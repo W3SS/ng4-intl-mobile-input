0c8983badc231f15e0cad87655d69bc9ead7e55d
Mahmoud Rabie <mahmoudrabie@Mahmouds-MacBook-Pro.local>
2017-07-18 21:26:35 +0300

    commit

--- src/app/intl-mobile/intl-mobile.component.ts
+++ src/app/intl-mobile/intl-mobile.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild, Input } from '@angular/core';
+import { Component, ElementRef, ViewChild, Input, HostListener } from '@angular/core';
 
 declare var jQuery: any, intlTelInput: any, intlTelInputUtils: any;
 
@@ -9,31 +9,47 @@ declare var jQuery: any, intlTelInput: any, intlTelInputUtils: any;
 })
 export class IntlMobileComponent{
 
-  private phoneNum = "";
+  public phoneNum = "";
+  	private validmsg: any;
+	private errormsg: any;
 	private el: HTMLElement;
 	private elInput;
     constructor(el: ElementRef) {
     	this.el = el.nativeElement;
-    }
+	}
+	
+	@HostListener('document:keyup', ['$event'])
+	onKeyUp(ev: KeyboardEvent) {
+		// do something meaningful with it
+		console.log(`The user just pressed ${ev.key}!`);
+		this.getNumber();
+	}
+
     ngAfterViewInit() {
-    	this.elInput = jQuery(this.el).find('.ng2intltelinput');
-	    this.elInput.intlTelInput();
+    	this.elInput = jQuery(this.el).find('.intl-mobile');
+		this.validmsg = jQuery(this.el).find('#valid-msg');
+		this.errormsg = jQuery(this.el).find('#error-msg');
+		this.elInput.intlTelInput();
     }
     private _getNumber(){
     	return this.elInput.intlTelInput('getNumber').replace(/[^\d]/, '');
     }
-	getNumber(){
-		var _phoneNum = this._getNumber();
-		var _isValid = this.validator();
-		if (_isValid){
+	getNumber() {
+		let _phoneNum = this._getNumber();
+		let _isValid = this.validator();
+		if (_isValid) {
 			this.phoneNum = _phoneNum;
 			this.elInput.css("border", "1px solid #00FF00");
+			this.validmsg.removeClass("hide");
+			this.errormsg.addClass("hide");
 			console.log(_phoneNum);
 			return _phoneNum;
 		}
 		else {
 			console.log('Invalid');
 			this.elInput.css("border", "1px solid #FF0000");
+			this.errormsg.removeClass("hide");
+			this.validmsg.addClass("hide");
 			return 'Invalid';
 		}
 	}
