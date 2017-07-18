601acdb68d6bd89db9881d5ceb1331968661adf2
Mahmoud Rabie <mahmoudrabie@Mahmouds-MacBook-Pro.local>
2017-07-18 19:39:44 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IntlMobileComponent } from './intl-mobile/intl-mobile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntlMobileComponent
   ],
   imports: [
     BrowserModule
